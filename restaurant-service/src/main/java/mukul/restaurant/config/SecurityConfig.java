76a5d503b7824eecb95eee46a42c83e9e63a7229
Mukul <mukul422003@gmail.com>
2026-05-28 23:21:41 +0530

    added restaurant-service gradle and config file.

---
+++ restaurant-service/src/main/java/mukul/restaurant/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package mukul.restaurant.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableMethodSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(
+            HttpSecurity http
+    ) throws Exception {
+
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().authenticated()
+                );
+
+        return http.build();
+    }
+}
