052105a7730e53d9d0c71afd66be8ff812a5968e
Mukul <mukul422003@gmail.com>
2026-06-17 21:28:06 +0530

    added jwt in delivery service and order service.

---
+++ delivery-service/src/main/java/mukul/deliveryservice/utils/JwtUtil.java
@@ -0,0 +1,49 @@
+package mukul.deliveryservice.utils;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+
+@Component
+public class JwtUtil {
+
+    private final String SECRET = "mukul-food-delivery-application-secret-key-2026";
+
+    private Key getKey() {
+        return Keys.hmacShaKeyFor(
+                SECRET.getBytes()
+        );
+    }
+
+    public String extractUsername(String token) {
+
+        return Jwts.parserBuilder()
+                .setSigningKey(getKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+
+    public String extractRole(String token) {
+
+        return Jwts.parserBuilder()
+                .setSigningKey(getKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .get("role", String.class);
+    }
+
+    public boolean validateToken(String token) {
+
+        Jwts.parserBuilder()
+                .setSigningKey(getKey())
+                .build()
+                .parseClaimsJws(token);
+
+        return true;
+    }
+}
--- payment-service/build.gradle.kts
+++ payment-service/build.gradle.kts
@@ -16,6 +16,8 @@ dependencies {
 
     implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
 
+    implementation("org.springframework.boot:spring-boot-starter-security")
+
     implementation("org.springframework.boot:spring-boot-starter-web")
 
     compileOnly("org.projectlombok:lombok")
@@ -29,6 +31,10 @@ dependencies {
 
     implementation("org.springframework.kafka:spring-kafka:3.0.8")
 
+    //jwt
+    implementation("io.jsonwebtoken:jjwt-api:0.11.5")
+    runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.5")
+    runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.5")
 }
 
 tasks.test {
